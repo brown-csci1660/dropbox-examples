0f320ecef475a0829bfb90748f8542ca0bb108bc
Zachary Espiritu <me@zacharyespiritu.com>
2021-03-22 21:11:12 -0400

    Update

--- test.py
+++ test.py
@@ -1,46 +1,58 @@
+##
+## test.py: Dropbox @ CSCI1660 (Spring 2021)
+##
+## This is the file where all of your test cases for your Dropbox client
+## implementation must go.
+##
+
+## WARNING: You MUST NOT change these default imports. If you change the default
+##          import statements in the stencil code, your implementation will be
+##          rejected by the autograder. (Our autograder actually enforces this
+##          this correctly, as opposed to the Crewmate Academy's autograder
+##          from the Handin project!)
+
 import unittest
 import string
-import random
-
-from client import *
 
 import support.crypto as crypto
 import support.util as util
 
-from support.dataserver import Dataserver, Memloc
-from support.keyserver import Keyserver
+from support.dataserver import dataserver, memloc
+from support.keyserver import keyserver
 
-import support.dataserver as ds
-import support.keyserver as ks
+from client import create_user, authenticate_user, User
 
-
-def new_config():
-    """ call this function at the beginning of tests to spawn a new enviroment """
-    ds.dataserver.data = {}
-    ks.keyserver.data = {}
-
+# DO NOT EDIT ABOVE THIS LINE ##################################################
 
 class ClientTests(unittest.TestCase):
+    def setUp(self):
+        """
+        This function is automatically called before every test is run. It clears the
+        dataserver and keyserver to a clean state for each test case.
+        """
+        dataserver.Clear()
+        keyserver.Clear()
 
     def test_create_user(self):
-        """Checks user creation"""
-        new_config()
+        """
+        Checks user creation.
+        """
         u = create_user("usr", "pswd")
         u2 = authenticate_user("usr", "pswd")
-        
+
         self.assertEqual(vars(u), vars(u2))
 
-
     def test_upload(self):
-        """Tests if uploading a file throws any errors"""
-        new_config()
+        """
+        Tests if uploading a file throws any errors.
+        """
         u = create_user("usr", "pswd")
         u.upload_file("file1", b'testing data')
 
     def test_download(self):
-        """Tests if a downloaded file has the correct data in it"""
-        new_config()
-
+        """
+        Tests if a downloaded file has the correct data in it.
+        """
         u = create_user("usr", "pswd")
 
         data_to_be_uploaded = b'testing data'
@@ -50,42 +62,42 @@ class ClientTests(unittest.TestCase):
 
         self.assertEqual(downloaded_data, data_to_be_uploaded)
 
-      
     def test_share_and_download(self):
-        """Simple test of sharing and downloading a shared file"""
-        new_config()
-
+        """
+        Simple test of sharing and downloading a shared file.
+        """
         u1 = create_user("usr1", "pswd")
         u2 = create_user("usr2", "pswd")
 
         u1.upload_file("shared_file", b'shared data')
         u1.share_file("shared_file", "usr2")
 
-        u2.recieve_file("shared_file", "usr1")
+        u2.receive_file("shared_file", "usr1")
         down_data = u2.download_file("shared_file")
 
         self.assertEqual(down_data, b'shared data')
-    
-    def test_download_error(self):
-      """Simple test that should throw an error"""
-      new_config()
-      u = create_user("usr", "pswd")
-      # file1 does not exist. Note that the function has to be wrapped in a lambda function
-      # when using AssertRaises
-      self.assertRaises(util.DropboxError, lambda: u.download_file("file1"))
 
+    def test_download_error(self):
+        """
+        Simple test that tests that downloading a file that doesn't exist
+        raise an error.
+        """
+        u = create_user("usr", "pswd")
+
+        # NOTE: When using `assertRaises`, the code that is expected to raise an
+        #       error needs to be passed to `assertRaises` as a lambda function.
+        self.assertRaises(util.DropboxError, lambda: u.download_file("file1"))
 
     def test_the_next_test(self):
-          """
-          Implement more tests by defining more functions like this one!
-          Note that functions have to start with the word "test" to be recognized. 
-          They can use a "self.Assert___" statement or can just run a series of commands. 
-          If there is no Assert statment, then the test will fail if and only if an error is raised. 
-          """
-          self.assertTrue(True)
+        """
+        Implement more tests by defining more functions like this one!
+        Note that functions have to start with the word "test" to be recognized.
+        They can use a "self.Assert___" statement or can just run a series of commands.
+        If there is no Assert statment, then the test will fail if and only if an error is raised.
+        """
+        self.assertTrue(True)
 
-
-      
+# DO NOT EDIT BELOW THIS LINE ##################################################
 
 if __name__ == '__main__':
     unittest.main()
